d1bc0c7647d6387811b5138a0b745a9b7c78acf8
C4PC15 <C4PC15@C4PC15>
2024-02-07 21:12:21 +0200

    Добавьте файлы проекта.

---
+++ Lesson_91/Models/Passengers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Controls;
+
+namespace Lesson_91.Models
+{
+    public class Passengers
+    {
+        public string PassengerId { get; set; }
+        public string Name { get; set; }
+        public string PClass { get; set; }
+        public string Age { get; set; }
+        public string Sex { get; set; }
+        public string Survived { get; set; }
+        public string SexCode { get; set; }
+
+    }
+}
---
+++ Lesson_91/Services/CSVFileReader.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using Lesson_91.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Lesson_91.Services
+{
+    public class CSVFileReader
+    {
+        public static List<Passengers> GetPassengers()
+        {
+            List<Passengers> titanicList;
+
+            using (StreamReader reader = new StreamReader("titanic.csv"))
+            {
+                CsvReader readCsv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+                titanicList = readCsv.GetRecords<Passengers>().ToList();
+            }
+            return titanicList;
+        }
+    }
+}
+
+
+
+
+
